0dbb1d3732577198780c2df8e988ba3d0fbcff0f
Flavio Gioia <flaviog2@hotmail.it>
2023-03-29 22:57:02 +0200

    ex.4

--- Esercizi JS/exercises.js
+++ Esercizi JS/exercises.js
@@ -19,3 +19,30 @@ function simpleArraySum(ar) {
 function simpleArraySum(ar) {
     return ar.reduce((total, element) => total + element);
 }
+
+// esercizio 3:
+function staircase(n) {
+    for (let i=0; i<n; i++) {
+        let line = '';
+        for(let k=1; k<(n-i); k++){
+            line += " ";
+        }
+        for(let j=0; j<=i; j++){
+            line += '#';
+        }
+        console.log(line);
+    }
+}
+
+// esercizio 4:
+function divisibleSumPairs(n, k, ar) {
+    let count = 0;
+    for(let i=0; i<n; i++){
+        for(let j = i+1; j<n; j++){
+            if((ar[i]+ar[j])%k === 0){
+                count+=1;
+            }
+        }
+    }
+    return count;
+}
