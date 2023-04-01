2b089a09a60ab472d72f6466bf4fdaa1433afe36
Flavio Gioia <flaviog2@hotmail.it>
2023-04-01 18:39:06 +0200

    lista della spesa brutta ma funzionante

--- Esercizi JS vite/Esercizio lista della spesa/main.js
+++ Esercizi JS vite/Esercizio lista della spesa/main.js
@@ -1,1 +1,26 @@
-import './style.css'
+const title = document.createElement('h1');
+const input = document.createElement('input');
+const addButton = document.createElement('button');
+
+title.textContent = "Lista della Spesa";
+addButton.textContent = "Aggiungi"
+
+document.body.appendChild(title);
+document.body.appendChild(input);
+document.body.appendChild(addButton);
+
+const lista = document.createElement('ul');
+document.body.appendChild(lista);
+
+addButton.addEventListener('click', () => {
+  const listElement = document.createElement('li');
+  listElement.textContent = input.value;
+  listElement.addEventListener('click', () => {
+    listElement.setAttribute('style', 'text-decoration: line-through');
+    listElement.addEventListener('dblclick', () => {
+      listElement.remove();
+    })
+  })
+  input.value = "";
+  document.body.appendChild(listElement);
+})
