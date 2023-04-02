df8628146b7f8c5f4f550ab2577365b655ae514e
Flavio Gioia <flaviog2@hotmail.it>
2023-04-02 21:06:44 +0200

    object of call is undefined

--- Esercizi JS vite/Tabella utenti/main.js
+++ Esercizi JS vite/Tabella utenti/main.js
@@ -1,32 +1,48 @@
 const title = document.createElement('h1');
 const userTable = document.createElement('table');
 
-const promiseHead = ['Nome', 'Username', 'Email', 'Phone'];
-let promise = []; 
+const dbKeys = ['Nome', 'Username', 'Email', 'Phone'];
+let dbData = []; 
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => response.json())
 .then((users) => {for (const user of users) {
-  let filter = [];
-  filter.push(user.name);
-  filter.push(user.username);
-  filter.push(user.email);
-  filter.push(user.phone);
-  promise.push(filter);
+  let filter = [{Nome: '', Username: '', Email: '', Phone: ''}];
+  filter[0].Nome = user.name;
+  filter[0].Username = user.username;
+  filter[0].Email = user.email;
+  filter[0].Phone = user.phone;
+  dbData.push(filter);
 }});
 
 title.textContent = "Lista Utenti:";
 document.body.appendChild(title);
 document.body.appendChild(userTable);
 
-function createTableHead(promiseHead, table) {
+function createTableHead(dbKeys, table) {
   let tableHead = table.createTHead();
   let headContent = tableHead.insertRow();
-  for (let key of promiseHead){
+  for (let key of dbKeys){
     const th = document.createElement('th');
     th.textContent = key;
     headContent.appendChild(th);
   }
 }
 
-createTableHead(promiseHead, userTable);
+function createTableBody(dbData, table) {
+  let dbKeys = Object.keys(dbData[0])
+  for (let user of dbKeys){
+    let row = table.insertRow();
+    for (data in user){
+      let cell = row.insertCell();
+      cell.textContent = data;
+    }
+  }
+}
+
+createTableHead(dbData, userTable);
+createTableBody(dbData, userTable);
+
+console.log(dbData);
+
+
