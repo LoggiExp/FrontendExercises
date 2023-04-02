a0065ae0dc745e9ab117b256cdbe0bf74dd4a162
Flavio Gioia <flaviog2@hotmail.it>
2023-04-02 22:07:05 +0200

    working table

--- Esercizi JS vite/Tabella utenti/main.js
+++ Esercizi JS vite/Tabella utenti/main.js
@@ -1,48 +1,49 @@
 const title = document.createElement('h1');
 const userTable = document.createElement('table');
 
-const dbKeys = ['Nome', 'Username', 'Email', 'Phone'];
-let dbData = []; 
-
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) => response.json())
-.then((users) => {for (const user of users) {
-  let filter = [{Nome: '', Username: '', Email: '', Phone: ''}];
-  filter[0].Nome = user.name;
-  filter[0].Username = user.username;
-  filter[0].Email = user.email;
-  filter[0].Phone = user.phone;
-  dbData.push(filter);
-}});
-
 title.textContent = "Lista Utenti:";
 document.body.appendChild(title);
 document.body.appendChild(userTable);
 
-function createTableHead(dbKeys, table) {
+function createTableHead(table) {
   let tableHead = table.createTHead();
   let headContent = tableHead.insertRow();
-  for (let key of dbKeys){
-    const th = document.createElement('th');
-    th.textContent = key;
-    headContent.appendChild(th);
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => Object.keys(json[0]))
+  .then((dbKeys) => [dbKeys[1], dbKeys[2], dbKeys[3], dbKeys[5]])
+  .then((dbKeys) => {
+    for (let key of dbKeys){
+      const th = document.createElement('th');
+      th.textContent = key;
+      headContent.appendChild(th);
+    }})
   }
-}
 
-function createTableBody(dbData, table) {
-  let dbKeys = Object.keys(dbData[0])
-  for (let user of dbKeys){
-    let row = table.insertRow();
-    for (data in user){
-      let cell = row.insertCell();
-      cell.textContent = data;
+function createTableBody(table) {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((dbData) => {
+    let filter = [];
+    for (let user of dbData){
+      let filtereduser = [user.name, user.username, user.email, user.phone];
+      filter.push(filtereduser);  
     }
-  }
+    return filter;
+  })
+  .then((filter) =>{
+    for (let userIndex in filter) {
+      let row = table.insertRow();
+      let user = filter[userIndex]
+      for(let dataindex in user) {
+      let cell = row.insertCell();
+      cell.textContent = user[dataindex];
+    }
+    }
+  })
 }
 
-createTableHead(dbData, userTable);
-createTableBody(dbData, userTable);
-
-console.log(dbData);
+createTableHead(userTable);
+createTableBody(userTable);
 
 
