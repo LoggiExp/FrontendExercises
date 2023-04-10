fa75db26f331ab962ede0e9f3116abfbee2a4f48
Flavio Gioia <flaviog2@hotmail.it>
2023-04-10 23:10:01 +0200

    remove with dblclick done, working on check

--- Angular project/TodoList/src/app/app.component.ts
+++ Angular project/TodoList/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,22 @@ export class AppComponent {
   title = 'todoList';
   value = '';
   list: string[] = [];
-
+  checkedState = false;
+  
   addToList(value: string) {
     this.list.push(value);
     this.value = '';
+  }
+  
+  listRemove(element: string) {
+    for(let i = 0; i < this.list.length; i++){
+      if (this.list[i] === element){
+        this.list = this.list.slice(0, i).concat(this.list.slice(i + 1))
+      }
+    }
+  }
+
+  isChecked(){
+    this.checkedState = !this.checkedState;
   }
 }
