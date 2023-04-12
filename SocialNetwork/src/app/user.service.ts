7199f7f0ef5f69633ed52dcc22c94235819e0b82
Flavio Gioia <flaviog2@hotmail.it>
2023-04-12 15:19:14 +0200

    service methods added

--- SocialNetwork/src/app/user.service.ts
+++ SocialNetwork/src/app/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,13 @@ export class UserService {
 
   getUsers() {
     return this.httpClient.get<JSON>(this.USERS).subscribe();
+  }
+
+  getUsersbyId(id: number) {
+    return this.httpClient.get<JSON>("${this.USERS}/id").subscribe();
+  }
+
+  getPostsbyId(id: number) {
+    return this.httpClient.get<JSON>(this.POSTS).pipe(filter((post) => post.userId === id));
   }
 }
