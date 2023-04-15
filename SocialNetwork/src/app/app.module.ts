9ea27514e915560d7379b991fe015d93ab27f4f3
Flavio Gioia <flaviog2@hotmail.it>
2023-04-15 18:12:40 +0200

    login component done

--- SocialNetwork/src/app/app.module.ts
+++ SocialNetwork/src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProfileComponent } from './profile/profile.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent, ProfileComponent, LoginComponent],
@@ -17,6 +22,10 @@ import { LoginComponent } from './login/login.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- SocialNetwork/src/app/login/login.component.ts
+++ SocialNetwork/src/app/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginControl = new FormGroup({
+    emailControl: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    passwordControl: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
 
+  login() {
+    console.log(this.loginControl.value);
+  }
 }
