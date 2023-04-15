8c8d6816d8eb2e940f4c9228bb8692abde0aafdd
Flavio Gioia <flaviog2@hotmail.it>
2023-04-15 14:23:25 +0200

    profile and post done, setup login

--- SocialNetwork/src/app/app.module.ts
+++ SocialNetwork/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ProfileComponent],
+  declarations: [AppComponent, ProfileComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
